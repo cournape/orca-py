cf491332825dbb20ac3433ef090ce1a8f70618f7
David Cournapeau <cournape@gmail.com>
2020-09-25 10:03:17 +0900

    FEAT: implement basic OrcaGrid.
    We implement basic peek/poke, and a simple orca-compat layer to help
    re-implementing some operators in the future.

--- src/orca/grid.py
+++ src/orca/grid.py
@@ -6,9 +6,42 @@ COMMENT_GLYPH = "#"
 EMPTY_GLYPH = " "
 
 GLYPH_TABLE = [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', #  0-11
-    'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', # 12-23
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', # 24-35
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",  #  0-11
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",  # 12-23
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",  # 24-35
 ]
 GLYPH_TABLE_SIZE = len(GLYPH_TABLE)
 INDEX_TO_GLYPH = {k: i for i, k in enumerate(GLYPH_TABLE)}
@@ -27,3 +60,86 @@ def glyph_to_value(glyph):
         return 0
     else:
         return glyph_table_index_of(glyph)
+
+
+class OrcaGrid:
+    """The internal orca grid representation.
+
+    It keeps track of the internal grid state, the locks.
+    """
+
+    @classmethod
+    def from_string(cls, s):
+        lines = s.splitlines()
+        if len(lines) > 200:
+            raise ValueError(f"String has too many lines ({len(lines)}, max is 200)")
+
+        if len(lines) < 1:
+            raise ValueError(f"Empty string !")
+
+        n = len(lines[0])
+        for i, line in enumerate(lines):
+            if len(line) != n:
+                raise ValueError(f"Line {i} length is inconsistent: {len(line)} vs {n}")
+
+        lines = [list(line) for line in lines]
+
+        ret = cls(len(lines), len(lines[0]))
+        ret._state = lines
+        return ret
+
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+
+        # Raw state of the grid at a given state
+        self._state = [[DOT_GLYPH for _ in range(cols)] for _ in range(rows)]
+
+        # Each grid's position may be locked, in which case it should not be
+        # processed as an operator. For example, if the right operator of add
+        # is itself a letter, you want to make sure you're not considering it
+        # as its own operator. Here we rely on orca processing from the left to
+        # the right, and from the top to the bottom.
+        self._locks = None
+
+        self.reset_locks()
+
+    def reset_locks(self):
+        """ Reset locks.
+
+        Every internal lock will be set as unlock.
+        """
+        self._locks = [
+            [False for _ in range(self.cols)]
+            for _ in range(self.rows)
+        ]
+
+    def peek(self, x, y):
+        """Returns the glyph at the given indices.
+
+        Will return empty string if outside the grid boundaries.
+        """
+        try:
+            return self._state[y][x]
+        except IndexError:
+            return None
+
+    def poke(self, x, y, value):
+        """ Will set the given value at the given position in the grid.
+
+        Will do nothing if outside the grid boundaries.
+        """
+        try:
+            self._state[y][x] = value
+        except IndexError:
+            pass
+
+    # Orca-compat 'layer', to help porting from orca nodejs code.
+    def glyph_at(self, x, y):
+        return self.peek(x, y)
+
+    def value_at(self, x, y):
+        return glyph_to_value(self.peek(x, y))
+
+    def value_of(self, g):
+        return glyph_to_value(g)
--- tests/test_orca_grid.py
+++ tests/test_orca_grid.py
@@ -3,7 +3,7 @@ import unittest
 from hypothesis import given
 from hypothesis.strategies import characters, sampled_from
 
-from orca.grid import GLYPH_TABLE, DOT_GLYPH, BANG_GLYPH, glyph_to_value
+from orca.grid import GLYPH_TABLE, DOT_GLYPH, BANG_GLYPH, glyph_to_value, OrcaGrid
 
 
 # This contains the list of glyph that will give back a value.
@@ -36,3 +36,126 @@ def test_glyph_to_value_unexpected(c):
 @given(sampled_from(SPECIAL_GLYPHS))
 def test_glyph_to_value_special(c):
     assert glyph_to_value(c) == 0
+
+
+def test_create_grid_from_string_simple(self):
+    # Given
+    s = "...\n..."
+
+    # When
+    grid = OrcaGrid.from_string(s)
+
+    # Then
+    assert grid.rows == 2
+    assert grid.cols == 3
+
+
+def test_create_grid_from_string_simple():
+    # Given
+    s = "...\n..."
+
+    # When
+    grid = OrcaGrid.from_string(s)
+
+    # Then
+    assert grid.rows == 2
+    assert grid.cols == 3
+
+
+O = OrcaGrid.from_string
+
+
+class TestGridPeekAndPoke(unittest.TestCase):
+    def test_peek_in_bounds(self):
+        # Given
+        grid = O(".A.\n...")
+
+        # When
+        glyph = grid.peek(0, 0)
+
+        # Then
+        assert glyph == DOT_GLYPH
+
+        # When
+        glyph = grid.peek(1, 0)
+
+        # Then
+        assert glyph == "A"
+
+    def test_peek_out_of_bounds(self):
+        # Given
+        grid = O(".A.\n...")
+
+        # When
+        glyph = grid.peek(4, 4)
+
+        # Then
+        assert glyph is None
+
+    def test_poke(self):
+        # Given
+        grid = O(".A.\n...")
+
+        # When
+        glyph = grid.peek(1, 1)
+
+        # Then
+        assert glyph == DOT_GLYPH
+
+        # When
+        grid.poke(1, 1, "3")
+        glyph = grid.peek(1, 1)
+
+        # Then
+        assert glyph == "3"
+
+
+class TestGridCompatLayer(unittest.TestCase):
+    def test_glyph_at(self):
+        # Given
+        grid = O(".A.\n...")
+
+        # When
+        glyph = grid.glyph_at(0, 0)
+
+        # Then
+        assert glyph == DOT_GLYPH
+
+        # When
+        glyph = grid.glyph_at(1, 0)
+
+        # Then
+        assert glyph == "A"
+
+    @given(sampled_from(SPECIAL_GLYPHS))
+    def test_value_of(self, c):
+        # Given
+        grid = O(".")
+
+        # When
+        value = grid.value_of(c)
+
+        # Then
+        assert value == 0
+
+    def test_value_at(self):
+        # Given
+        grid = O("3")
+
+        # When
+        value = grid.value_at(0, 0)
+
+        # Then
+        assert value == 3
+
+        # When
+        value = grid.value_at(0, 3)
+
+        # Then
+        assert value == 0
+
+        # When
+        value = grid.value_at(-2, 1)
+
+        # Then
+        assert value == 0
